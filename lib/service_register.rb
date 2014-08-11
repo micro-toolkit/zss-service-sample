930c4b98f1f0cd83272949b9cddcf53eb82c9143
Pedro Januário <prnjanuario@gmail.com>
2014-08-11 12:54:42 +0100

    extract service registry to proper file

---
+++ lib/service_register.rb
@@ -0,0 +1,16 @@
+module ZSS
+  class ServiceRegister
+
+    def self.get_service
+      config = Hashie::Mash.new(
+        backend: 'tcp://127.0.0.1:7776'
+      )
+      service = ZSS::Service.new(:pong, config)
+
+      instance = PongService.new
+      service.add_route(instance, :ping)
+
+      service
+    end
+  end
+end
--- lib/start.rb
+++ lib/start.rb
@@ -1,19 +1,5 @@
 require 'zss/service'
+
+require_relative 'service_register'
 require_relative 'pong_service'
 
-module ZSS
-  class ServiceRegister
-
-    def self.get_service
-      config = Hashie::Mash.new(
-        backend: 'tcp://127.0.0.1:7776'
-      )
-      service = ZSS::Service.new(:pong, config)
-
-      instance = PongService.new
-      service.add_route(instance, :ping)
-
-      service
-    end
-  end
-end
