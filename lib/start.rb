290bd3597aa795da92179f6ce447f48564c37e86
Pedro Januário <prnjanuario@gmail.com>
2014-08-11 12:55:26 +0100

    register logging plugin

--- lib/start.rb
+++ lib/start.rb
@@ -3,3 +3,6 @@ require 'zss/service'
 require_relative 'service_register'
 require_relative 'pong_service'
 
+# register a plugin to see zss service logging
+plugin = LoggerFacade::Plugins::Console.new({ level: :debug })
+LoggerFacade::Manager.use(plugin)
